6e5d1de38908e1927b9af5fc7b1a887185332549
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-16 23:55:00 +0530

    Update hello.py

--- NumPyDemos/helloworld/hello.py
+++ NumPyDemos/helloworld/hello.py
@@ -4,4 +4,8 @@ import numpy as np  # import the NumPy library
 arr = np.array([-1, 2, 5], dtype=np.float32)
 
 # Print the representation of the array
-print(repr(arr))
+print('Single Dimension Array: ', repr(arr))
+
+arr = np.array([[0, 1, 2], [3, 4, 5]], dtype=np.float32)
+
+print('Double Dimension Array: ', repr(arr))
