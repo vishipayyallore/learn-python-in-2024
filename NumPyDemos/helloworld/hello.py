5cfc3703e1bb295c0124bfbfa3c961c013120b04
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-17 00:04:14 +0530

    Update hello.py

--- NumPyDemos/helloworld/hello.py
+++ NumPyDemos/helloworld/hello.py
@@ -13,3 +13,14 @@ print('Double Dimension Array: ', repr(arr))
 # When the elements of a NumPy array are mixed types, then the array's type will be upcast to the highest level type.
 arr = np.array([0, 0.1, 2])
 print('Single Dimension Array: ', repr(arr))
+
+a = np.array([0, 1])
+b = np.array([9, 8])
+c = a
+print('Array a: {}'.format(repr(a)))
+c[0] = 5
+print('Array a: {}'.format(repr(a)))
+
+d = b.copy()
+d[0] = 6
+print('Array b: {}'.format(repr(b)))
