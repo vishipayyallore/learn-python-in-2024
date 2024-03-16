ed3676a4eb739fa111c7eef9eb25b490e4cada40
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-17 00:01:17 +0530

    Update hello.py

--- NumPyDemos/helloworld/hello.py
+++ NumPyDemos/helloworld/hello.py
@@ -1,16 +1,15 @@
 import numpy as np  # import the NumPy library
 
+# NumPy arrays are basically just Python lists with added features.
+# dtype is optional. https://numpy.org/doc/stable/user/basics.types.html
+
 # Initializing a NumPy array
 arr = np.array([-1, 2, 5], dtype=np.float32)
-
-# Print the representation of the array
 print('Single Dimension Array: ', repr(arr))
 
 arr = np.array([[0, 1, 2], [3, 4, 5]], dtype=np.float32)
-
 print('Double Dimension Array: ', repr(arr))
 
+# When the elements of a NumPy array are mixed types, then the array's type will be upcast to the highest level type.
 arr = np.array([0, 0.1, 2])
-
 print('Single Dimension Array: ', repr(arr))
-
