9a121a1f96c3e450bb03207c5b911ed347ef0611
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-03-17 22:35:40 +0530

    Update hello.py

--- NumPyDemos/helloworld/hello.py
+++ NumPyDemos/helloworld/hello.py
@@ -3,21 +3,6 @@ import numpy as np  # import the NumPy library
 # NumPy arrays are basically just Python lists with added features.
 # dtype is optional. https://numpy.org/doc/stable/user/basics.types.html
 
-arr3 = np.array([10, 11, 12])
-print(repr(arr3), arr3.dtype)
-
-arr4 = arr3.astype(np.float32)
-print(repr(arr4), arr4.dtype)
-
-arr5 = np.array([np.nan, 1, 2])
-print(repr(arr5))
-
-arr6 = np.array([np.nan, 'abc'])
-print(repr(arr6))
-
-# np.array([np.nan, 1, 2], dtype=np.int32) # ValueError => cannot convert float NaN to integer
-np.array([np.nan, 1, 2], dtype=np.float32)
-
 
 def show_numpy_hw():
     print('\nShow NumPy Hello World')
@@ -52,6 +37,29 @@ def show_numpy_array_copy():
     print('Array b: {}'.format(repr(b)))
 
 
+def show_numpy_array_casting():
+    print('\nShow NumPy Array Casting')
+
+    arr3 = np.array([10, 11, 12])
+    print(repr(arr3), arr3.dtype)
+
+    arr4 = arr3.astype(np.float32)
+    print(repr(arr4), arr4.dtype)
+
+
+def show_numpy_array_nan():
+    print('\nShow NumPy Array NaN')
+
+    arr5 = np.array([np.nan, 1, 2])
+    print(repr(arr5))
+
+    arr6 = np.array([np.nan, 'abc'])
+    print(repr(arr6))
+
+    # np.array([np.nan, 1, 2], dtype=np.int32) # ValueError => cannot convert float NaN to integer
+    np.array([np.nan, 1, 2], dtype=np.float32)
+
+
 def show_infinity():
     print('\nShowing Infinity')
     print(np.inf)
@@ -74,5 +82,9 @@ show_numpy_hw()
 show_numpy_matrix()
 
 show_numpy_array_copy()
+
+show_numpy_array_casting()
+
+show_numpy_array_nan()
 
 show_infinity()
